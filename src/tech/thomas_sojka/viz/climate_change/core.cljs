d231d3beaa635c1d09b9f3fe8cfae2f57f62bc7b
Thomas Sojka <thsojka@web.de>
2022-07-20 07:47:16 +0200

    Basic data read

--- src/tech/thomas_sojka/viz/climate_change/core.cljs
+++ src/tech/thomas_sojka/viz/climate_change/core.cljs
@@ -3,35 +3,46 @@
             ["netcdfjs" :as netcdfjs]
             [cljs-bean.core :refer [bean ->clj]]))
 
-(defn main [& _cli-args]
-  (prn "hello world"))
+(defn index-by [f coll]
+  (-> (group-by f coll)
+      (update-vals #(dissoc (first %) f))
+      (update-keys keyword)))
+
+(defn variable-attributes [reader key]
+  (let [variableAttributes (index-by :name (->clj ^js (.-variables reader)))]
+    (index-by :name (:attributes (key variableAttributes)))))
+
+(defn fill-value [reader key]
+  (let [variable-attributes (variable-attributes reader key)]
+    (-> variable-attributes :_FillValue :value)))
+
+(defn scale-factor [reader key]
+  (let [variable-attributes (variable-attributes reader key)]
+    (-> variable-attributes :scale_factor :value)))
+
+(defn variables [reader]
+  (->> (:variables (bean (:header (bean reader))))
+       ->clj
+       (index-by :name)))
 
 (def reader
   (new netcdfjs/NetCDFReader
        (fs/readFileSync "./resources/gistemp250_GHCNv4.nc")))
+
+(defn temp-anamoly-for-time [point-in-time]
+  (for [[i anamoly] (->> (.getDataVariable reader "tempanomaly")
+                         (take (* 90 180 point-in-time))
+                         (map-indexed vector))
+        :let [lat (mod i 90)
+              lon (Math/floor (/ i 180))]
+        :when (not= anamoly (fill-value reader :tempanomaly))]
+    {:anomaly (* anamoly (scale-factor reader :tempanomaly))
+     :lat lat
+     :lon lon}))
+
+(defn main [& _cli-args]
+  (prn reader))
+
 (comment
-  (keys (->clj reader))
-  (map (comp :name bean) (:variables (bean (:header (bean reader)))))
-  (map (comp bean) (:variables (bean (:header (bean reader)))))
-  (mapv (comp #(select-keys % [:name :offset]) bean) (:variables (bean (:header (bean reader)))))
-  [{:name "lat", :offset 1100}
-   {:name "lon", :offset 1460}
-   {:name "time", :offset 2180}
-   {:name "time_bnds", :offset 9016}
-   {:name "tempanomaly", :offset 22688}]
-  (take 10 (.getDataVariable reader "tempanomaly"))
-  (take 10 (.getDataVariable reader "time"))
-  (take 10 (.getDataVariable reader "time_bnds"))
-
-  (.-dimensions reader)
-  (.-recordDimension reader)
-  (first (.-variables reader))
-  (.getDataVariableAsString reader "lat")
-  (.-globalAttributes reader)
-  (.getAttribute reader "tempanomaly")
-  (bean (:buffer (bean reader)))
-  ;; https://help.marine.copernicus.eu/en/articles/5470092-how-to-use-add_offset-and-scale_factor-values
-  ;; Real_Value = (Display_Value X scale_factor) + add_offset
-
-
-  )
+  (fs/writeFileSync "resources/data.edn" (prn-str (vec (temp-anamoly-for-time 1))))
+  (variables reader))
