8c6c967fdc6ce3c52d7c67ffff4b92b5e307bf87
Thomas Sojka <thsojka@web.de>
2022-07-31 10:47:50 +0200

    Randomize and nice colors

--- src/tech/thomas_sojka/viz/climate_change/app.cljs
+++ src/tech/thomas_sojka/viz/climate_change/app.cljs
@@ -2,6 +2,7 @@
   (:require
    ["d3-contour" :as d3-contour]
    ["d3-geo" :as d3-geo]
+   ["nice-color-palettes" :as palettes]
    [reagent.core :as r]
    [reagent.dom :as dom]))
 
@@ -12,59 +13,66 @@
   (r/atom
    [[5 2 1][2 2 1][1 1 1] [1 1 1]]))
 
-(def path (d3-geo/geoPath))
+(def thresholds [1 2 4 6 8])
 
-(defn contour-example [{:keys [add-row! add-column! data set-data!]}]
+(defn randomize [data] (map (fn [row] (map (fn [_] (rand-int 10)) row)) data))
+
+(def path (d3-geo/geoPath))
+(def palette-idx (r/atom 19))
+
+(defn contour-example [{:keys [add-row! add-column! data set-cell! update-data!]}]
   (let [contours (-> (d3-contour/contours)
                      (.size (clj->js [(count (first data)) (count data)])))]
-    [:div.flex.items-center
-     [:div
+    [:div
+     [:div.flex.items-center
       [:div
-       [:div.flex.flex-wrap.max-w-full.absolute
-        {:style {:width (* (count (first data)) (/ 500 (count data)))
-                 :height 500}}
-        (map-indexed
-         (fn [y row]
-           (map-indexed
-            (fn [x cell]
-              [:div.flex.flex-col.justify-center.items-center
-               {:style {:width (str (* 100 (/ 1 (count (first data)))) "%")
-                        :height (str (* 100 (/ 1 (count data))) "%")}}
-               [:input.bg-transparent.text-center
-                {:key [x y]
-                 :value cell
-                 :on-change (fn [e] (set-data! x y ^js (.-target.value e)))}]
-               [:input {:type "range"
-                        :min -10
-                        :max 10
-                        :value cell
-                        :on-change (fn [e] (set-data! x y ^js (.-target.value e)))}]])
-            row))
-         data)]
-       [:svg.max-w-full
-        {:viewBox (str 0 " " 0 " " (count (first data)) " " (count data))
-         :style {:height 500}}
-        [:g
+       [:div
+        [:div.flex.flex-wrap.max-w-full.absolute
+         {:style {:width (* (count (first data)) (/ 500 (count data)))
+                  :height 500}}
          (map-indexed
-          (fn
-            [i t]
-            [:path
-             {:key i
-              :d (path (.contour contours (clj->js (flatten data)) t))
-              :fill (nth ["green" "yellow" "red" "orange" "blue" "pink" "gold"] i)}])
-          [1 2 3])]]]
-      [:div.flex.justify-center
-       [:button.text-4xl {:on-click add-row!}"+"]]]
-     [:button.text-4xl {:on-click add-column!}"+"]]))
+          (fn [y row]
+            (map-indexed
+             (fn [x cell]
+               [:div.flex.flex-col.justify-center.items-center
+                {:style {:width (str (* 100 (/ 1 (count (first data)))) "%")
+                         :height (str (* 100 (/ 1 (count data))) "%")}}
+                [:input.bg-transparent.text-center.max-w-full.hidden
+                 {:key [x y]
+                  :value cell
+                  :on-change (fn [e] (set-cell! x y ^js (.-target.value e)))}]
+                [:input.max-w-full.hidden
+                 {:type "range"
+                  :min 0
+                  :max 9
+                  :value cell
+                  :on-change (fn [e] (set-cell! x y ^js (.-target.value e)))}]])
+             row))
+          data)]
+        [:svg.max-w-full
+         {:viewBox (str 0 " " 0 " " (count (first data)) " " (count data))
+          :style {:height 500}}
+         [:g
+          (map-indexed
+           (fn
+             [i t]
+             [:path
+              {:key i
+               :d (path (.contour contours (clj->js (flatten data)) t))
+               :fill (nth (nth palettes @palette-idx) i)}])
+           thresholds)]]]
+       [:div.flex.justify-center
+        [:button.text-4xl {:on-click add-row!}"+"]]]
+      [:button.text-4xl {:on-click add-column!}"+"]]
+     [:button {:on-click #(update-data! randomize)}"Randomize"]]))
 
 (defn app []
   [:div.m-8
    [contour-example {:data @data
                      :add-row! #(swap! data add-row)
                      :add-column! #(swap! data add-column)
-                     :set-data! (fn [x y value]
-                                  (prn x y (int value))
-                                  (swap! data assoc-in [y x] (int value)))}]])
+                     :update-data! (fn [f] (swap! data f))
+                     :set-cell! (fn [x y value] (swap! data assoc-in [y x] (int value)))}]])
 
 (dom/render
  [app]
