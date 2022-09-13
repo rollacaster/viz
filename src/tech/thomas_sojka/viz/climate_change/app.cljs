e61031d0fb81619fc7ba3d0489b0e4ca94b8c9cd
Thomas Sojka <thsojka@web.de>
2022-09-13 08:59:55 +0200

    Contour for temperature data

--- src/tech/thomas_sojka/viz/climate_change/app.cljs
+++ src/tech/thomas_sojka/viz/climate_change/app.cljs
@@ -1,8 +1,55 @@
 (ns tech.thomas-sojka.viz.climate-change.app
-  (:require
-   [tech.thomas-sojka.viz.climate-change.d3-countour-by-example :as d3-countour-by-example]
-   [reagent.dom :as dom]))
+  (:require ["d3-contour" :as d3-contour]
+            ["d3-geo" :as d3-geo]
+            ["nice-color-palettes" :as palettes]
+            [reagent.dom :as dom]
+            [reagent.ratom :as r]
+            [tech.thomas-sojka.viz.climate-change.data :as data]))
+
+(def data (r/atom nil))
+
+(->(js/fetch "/data/climate-change/gistemp250_GHCNv4.nc")
+   (.then (fn [res] (.arrayBuffer res)))
+   (.then (fn [d]
+            (reset! data (data/read d)))))
+
+(def path (d3-geo/geoPath))
+
+(def year (r/atom 0))
+
+(defn temperatur-contour []
+  (when @data
+    (let [width 180
+          height 360
+          data (update-vals (group-by (juxt :lat :lon) (data/temp-anamoly-for-time @data @year)) #(first (map :anomaly %)))
+          contour-data (for [lat (range -90 90)
+                             long (range -180 180)]
+                         (get data [lat long] 383838))
+          contours (-> (d3-contour/contours) (.size (clj->js [width height])))]
+      [:div
+       {:style {:width 500}}
+       [:input {:type "range" :min 0 :max 200 :value @year :on-change #(reset! year ^js (.-target.value %))}] @year
+       [:svg
+        {:viewBox (str 0 " " 0 " " width " " height)
+         :style {:transform "rotate(-90deg) translateX(-100%)"
+                 :transform-origin "top left"}}
+        [:g
+         (->> contour-data
+              into-array
+              contours
+              (map-indexed (fn [i c]
+                             [:path
+                              {:key i
+                               :value (.-value c)
+                               :d (path c)
+                               :fill
+                               (if (= (.-value c) 383838)
+                                 "rgba(127,127,127,0.5)"
+                                 (get (nth palettes 96)
+                                      (mod i 5)))}])))]]])))
+
+(defn main [])
 
 (dom/render
- [d3-countour-by-example/main]
+ [temperatur-contour]
  (js/document.getElementById "app"))
--- src/tech/thomas_sojka/viz/climate_change/core.cljs
+++ src/tech/thomas_sojka/viz/climate_change/core.cljs
@@ -1,48 +1,12 @@
 (ns tech.thomas-sojka.viz.climate-change.core
   (:require ["fs" :as fs]
             ["netcdfjs" :as netcdfjs]
-            [cljs-bean.core :refer [bean ->clj]]))
-
-(defn index-by [f coll]
-  (-> (group-by f coll)
-      (update-vals #(dissoc (first %) f))
-      (update-keys keyword)))
-
-(defn variable-attributes [reader key]
-  (let [variableAttributes (index-by :name (->clj ^js (.-variables reader)))]
-    (index-by :name (:attributes (key variableAttributes)))))
-
-(defn fill-value [reader key]
-  (let [variable-attributes (variable-attributes reader key)]
-    (-> variable-attributes :_FillValue :value)))
-
-(defn scale-factor [reader key]
-  (let [variable-attributes (variable-attributes reader key)]
-    (-> variable-attributes :scale_factor :value)))
-
-(defn variables [reader]
-  (->> (:variables (bean (:header (bean reader))))
-       ->clj
-       (index-by :name)))
+            [tech.thomas-sojka.viz.climate-change.data :as data]))
 
 (def reader
   (new netcdfjs/NetCDFReader
        (fs/readFileSync "./resources/gistemp250_GHCNv4.nc")))
 
-(defn temp-anamoly-for-time [point-in-time]
-  (for [[i anamoly] (->> (.getDataVariable reader "tempanomaly")
-                         (take (* 90 180 point-in-time))
-                         (map-indexed vector))
-        :let [lat (mod i 90)
-              lon (Math/floor (/ i 180))]
-        :when (not= anamoly (fill-value reader :tempanomaly))]
-    {:anomaly (* anamoly (scale-factor reader :tempanomaly))
-     :lat lat
-     :lon lon}))
-
-(defn main [& _cli-args]
-  (prn reader))
-
 (comment
-  (fs/writeFileSync "resources/data.edn" (prn-str (vec (temp-anamoly-for-time 1))))
-  (variables reader))
+  (fs/writeFileSync "resources/public/data/climate-change/1880.edn" (prn-str (vec (data/temp-anamoly-for-time reader 1))))
+  (data/variables reader))
