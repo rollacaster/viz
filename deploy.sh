b6d8568f1e219d38bf50acf3e75dabcb5507eb42
Thomas Sojka <thsojka@web.de>
2023-01-18 07:16:04 +0100

    More deploy updates

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+rm -rf resources/contour-by-example/js;
+rm -rf resources/climate-change/js;
+npx shadow-cljs release :climate-change-server;
+npx shadow-cljs release :climate-change;
+npx shadow-cljs release :contour-by-example;
+vercel;
