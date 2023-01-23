3185baa6b52a13cde27164b24132fba1678109ad
Thomas Sojka <thsojka@web.de>
2023-01-23 21:14:32 +0100

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 rm -rf resources/contour-by-example/js;
 rm -rf resources/climate-change/js;
-npx shadow-cljs release :climate-change-server;
 npx shadow-cljs release :climate-change;
 npx shadow-cljs release :contour-by-example;
 vercel;
