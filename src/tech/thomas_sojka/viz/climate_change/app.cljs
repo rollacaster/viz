db43ad8c8cac484f61d2c91470e69accaaef0e5a
Thomas Sojka <thsojka@web.de>
2023-01-23 20:38:10 +0100

    Animated transitions

--- src/tech/thomas_sojka/viz/climate_change/app.cljs
+++ src/tech/thomas_sojka/viz/climate_change/app.cljs
@@ -1,8 +1,11 @@
 (ns tech.thomas-sojka.viz.climate-change.app
-  (:require ["d3-contour" :as d3-contour]
+  (:require ["box-intersect" :as box-intersect]
+            ["d3-contour" :as d3-contour]
             ["d3-geo" :as d3-geo]
             ["d3-scale" :as d3-scale]
             ["d3-scale-chromatic" :as d3-scale-chromatic]
+            ["flubber" :as flubber]
+            ["lodash" :as lodash]
             ["topojson-client" :as topojson]
             [goog.string :as gstring]
             [goog.string.format]
@@ -12,9 +15,11 @@
 
 (defonce surface-temperature-data (r/atom nil))
 (defonce geojson-world (r/atom nil))
-(defonce year (r/atom 0))
+(defonce inputs (r/atom {:year 0
+                         :value-filter 14}))
 
-(def projection (d3-geo/geoMercator))
+(def projection (-> (d3-geo/geoMercator)
+                    (.translate #js [480 300])))
 (def path (d3-geo/geoPath projection))
 
 (def lng-scale
@@ -27,55 +32,201 @@
       (.domain #js [0 90])
       (.range #js [-90 90])))
 
-(def contours (-> (d3-contour/contours) (.size (clj->js [180 90])) (.thresholds (clj->js (range 0 21)))))
+(def contours (-> (d3-contour/contours)
+                  (.size (clj->js [180 90]))
+                  (.thresholds (clj->js (range 0 21)))))
 (defn contours-at-time [surface-temperature-data time]
   (->> time (aget surface-temperature-data) into-array contours))
+
+(defn- polygon-bounds [polygon]
+  (-> path
+      (.bounds #js {:type "Polygon" :coordinates polygon})
+      (.flat)))
+
+(defn- render-ring []
+  (let [start (.now js/Date)
+        time (r/atom 0)
+        duration 500]
+    (letfn [(on-frame []
+              (when (< @time duration)
+                (reset! time (- (.now js/Date) start))
+                (on-loop)))
+            (on-loop [] (js/requestAnimationFrame on-frame))]
+      (on-loop)
+      (fn [{:keys [value fill label d next-d center]}]
+        [:<>
+         [:path.contour
+          {:value value
+           :on-click #(prn value)
+           :d ((flubber/interpolate d next-d)
+               (/ @time duration))
+           :fill fill}]
+         (let [[x y] center]
+           [:text.fill-red-700.font-bold {:x x :y y} label])]))))
+
+(defn- render-polygon [{:keys [polygon next-polygon render-ring]}]
+  [:<>
+   (map
+    (fn [[k ring-path] next-ring-path]
+      ^{:key k}
+      [render-ring {:d ring-path
+                    :next-d next-ring-path
+                    :center (.centroid path #js {:type "Polygon" :coordinates polygon})}])
+    (map-indexed vector polygon)
+    next-polygon)])
+
+(defn- polygons-with-next [multi-polygon next-contour intersections]
+  (->> (range (count (.-coordinates next-contour)))
+       (map (fn [next-polygon-idx]
+              (if ((group-by second intersections) next-polygon-idx)
+                (let [[[polygon-idx]] ((group-by second intersections) next-polygon-idx)]
+                  {:type :update
+                   :data
+                   {:from (get (.-coordinates multi-polygon) polygon-idx)
+                    :to (get (.-coordinates next-contour) next-polygon-idx)}})
+                {:type :new
+                 :data (get (.-coordinates multi-polygon) next-polygon-idx)})))))
+
+(defn- render-animated-contours [{:keys [contour color-scale next-contour]}]
+  (->> contour
+       (keep-indexed (fn [i multi-polygon]
+                       (when (and
+                              (or (= (:value-filter @inputs) (.-value multi-polygon))
+                                  (nil? (:value-filter @inputs)))
+                              (not= (.-value multi-polygon) 0))
+                         (let [intersections
+                               (if next-contour
+                                 (box-intersect
+                                  (into-array (map polygon-bounds (.-coordinates multi-polygon)))
+                                  (into-array (map polygon-bounds (.-coordinates (get next-contour i)))))
+                                 #js [])]
+                           (->> intersections
+                                (polygons-with-next multi-polygon (get next-contour i))
+                                (map-indexed vector)
+                                (map
+                                 (fn [[j {:keys [type data]}]]
+                                   (case type
+                                     :update (let [{:keys [from to]} data]
+                                               ^{:key j}
+                                               [render-polygon {:polygon from
+                                                                :next-polygon to
+                                                                :render-ring
+                                                                (fn [{:keys [d next-d center]}]
+                                                                  [render-ring {:value (.-value multi-polygon)
+                                                                                :fill (color-scale (/ i (count contour)))
+                                                                                :center center
+                                                                                :label j
+                                                                                :d (path #js {:type "Polygon" :coordinates #js [d]})
+                                                                                :next-d (path #js {:type "Polygon" :coordinates #js [next-d]})}])}])
+                                     :new
+                                     ^{:key j}
+                                     [render-polygon {:polygon data
+                                                      :next-polygon data
+                                                      :render-ring
+                                                      (fn [{:keys [d next-d center]}]
+                                                        [render-ring {:value (.-value multi-polygon)
+                                                                      :fill (color-scale (/ i (count contour)))
+                                                                      :center center
+                                                                      :label j
+                                                                      :d (path #js {:type "Polygon" :coordinates #js [d]})
+                                                                      :next-d (path #js {:type "Polygon" :coordinates #js [next-d]})}])}]))))))))))
+
+(defn- render-contours [{:keys [contour color-scale]}]
+  (->> contour
+       (keep-indexed (fn [i multi-polygon]
+                       (when (and
+                              (or (= (:value-filter @inputs) (.-value multi-polygon))
+                                  (nil? (:value-filter @inputs)))
+                              (not= (.-value multi-polygon) 0))
+                         (->> (.-coordinates multi-polygon)
+                              (map-indexed vector)
+                              (map
+                               (fn [[j polygon]]
+                                 ^{:key j}
+                                 [render-polygon {:polygon polygon
+                                                  :next-polygon polygon
+                                                  :render-ring
+                                                  (fn [{:keys [d next-d center]}]
+                                                    [render-ring {:value (.-value multi-polygon)
+                                                                  :fill (color-scale (/ i (count contour)))
+                                                                  :center center
+                                                                  :label j
+                                                                  :d (path #js {:type "Polygon" :coordinates #js [d]})
+                                                                  :next-d (path #js {:type "Polygon" :coordinates #js [next-d]})}])}]))))))))
+
+(defn- fix-coords [next-contour]
+  (doseq [multi-polygon next-contour]
+       (set! (.-coordinates multi-polygon)
+             (.map (.-coordinates multi-polygon)
+                   (fn [polygon]
+                     (.map polygon
+                           (fn [ring]
+                             (.map ring (fn [[lng lat]] #js [(lng-scale lng) (lat-scale lat)])))))))))
+
 (defn temperatur-contour [{:keys [surface-temperature-data geojson-world]}]
-  (let [data (aget surface-temperature-data @year)
-        contours (-> (d3-contour/contours) (.size (clj->js [180 90])) (.thresholds (clj->js (range -10 11))))
-        c (->> data into-array contours)]
+  (let [{:keys [value-filter year]} @inputs
+        c (contours-at-time surface-temperature-data year)
+        next-contour (contours-at-time surface-temperature-data (inc year))]
     [:div
-     [:input {:type "range" :min 0 :max 1704 :value @year :on-change #(reset! year (int ^js (.-target.value %)))}]
-     [:span (str (+ 1880 (js/Math.floor (/ (int @year) 12)))
+     [:input {:type "range" :min 0 :max 1704 :value year :on-change #(swap! inputs assoc :year (int ^js (.-target.value %)))}]
+     [:span (str (+ 1880 (js/Math.floor (/ (int year) 12)))
                  "-"
-                 (gstring/format "%02d" (inc (mod @year 12))))]
+                 (gstring/format "%02d" (inc (mod year 12))))]
      [:button.bg-gray-200.p-1.rounded.ml-2 {:on-click (fn []
                                                         (doseq [i (range 0 1709)]
                                                           (js/setTimeout
-                                                           (fn [] (swap! year inc)) (* i 130))))}
+                                                           (fn [] (swap! inputs update :year inc)) (* i 400))))}
       "Play"]
      [:style {:dangerouslySetInnerHTML {:__html ".contour:hover {fill: red}"}}]
-     [:div.flex.items-center
+     [:div.flex.items-center.mb-4
       (map-indexed (fn [i multi-polygon]
                      ^{:key (.-value multi-polygon)}
-                     [:div.flex.mr-2.items-center
+                     [:div.flex.mr-1.items-center
                       [:div.mr-1 (- (.-value multi-polygon) 10)]
-                      [:span.inline-block.w-4.h-4 {:style {:background-color (.interpolateMagma d3-scale-chromatic (/ i (count c)))}}]])
+                      [:span.inline-block.w-3.h-3.rounded {:style {:background-color (.interpolateTurbo d3-scale-chromatic (/ i (count c)))}}]])
                    c)]
-     [:svg {:width 960 :height 500 :viewBox "0 0 960 500"}
-      [:g [:path {:opacity 0.5 :d (path geojson-world)}]]
-      [:g {:opacity 0.7}
-       (->> c
-            (map-indexed (fn [i multi-polygon]
-                           (set! (.-coordinates multi-polygon)
-                                 (.map (.-coordinates multi-polygon)
-                                       (fn [polygon]
-                                         (.map polygon
-                                               (fn [ring]
-                                                 (.map ring
-                                                       (fn [[lng lat]] #js [(lng-scale lng) (lat-scale lat)])))))))
-                           (map
-                            (fn [[j polygon]]
-                              (map-indexed
-                               (fn [k ring-path]
-                                 [:path.contour
-                                  {:key (str (.-value multi-polygon) " " j " " k)
-                                   :value (str (.-value multi-polygon) " " j "" k)
-                                   :on-click #(prn (.-value multi-polygon))
-                                   :d (str ring-path "Z")
-                                   :fill (.interpolateMagma d3-scale-chromatic (/ i (count c)))}])
-                               (str/split (path #js {:type "Polygon" :coordinates polygon}) #"Z")))
-                            (map-indexed vector (.-coordinates multi-polygon))))))]]]))
+     [:div.flex.mb-4
+      (map (fn [multi-polygon]
+             ^{:key (.-value multi-polygon)}
+             [:div.flex.mr-1.items-center
+              [:button.p-1.rounded
+               {:class (if (= value-filter (.-value multi-polygon))
+                         "bg-blue-400 text-white" "bg-gray-100")
+                :on-click #(swap! inputs assoc :value-filter
+                                  (when-not (= value-filter (.-value multi-polygon))
+                                    (.-value multi-polygon)))}
+               (.-value multi-polygon)]])
+           c)]
+     (fix-coords c)
+     (fix-coords next-contour)
+     (let [[width height] (map #(/ % 1.3) [960 500])]
+       [:<>
+        [:svg {:width width :height height :viewBox (str "0 0 "960 " " 500)}
+         [:g [:path {:opacity 0.5 :d (path geojson-world)}]]
+         [:g {:opacity 0.7}
+          (doall
+           (render-animated-contours
+            {:contour c
+             :next-contour next-contour
+             :color-scale #(.interpolateTurbo d3-scale-chromatic %)}))]]
+        year
+
+        [:svg {:width width :height height :viewBox (str "0 0 "960 " " 500)}
+         [:g [:path {:opacity 0.5 :d (path geojson-world)}]]
+         [:g {:opacity 0.7}
+          (doall
+           (render-contours
+            {:contour c
+             :color-scale #(.interpolateTurbo d3-scale-chromatic %)}))]]
+        year
+        [:svg {:width width :height height :viewBox (str "0 0 "960 " " 500)}
+         [:g [:path {:opacity 0.5 :d (path geojson-world)}]]
+         [:g {:opacity 0.7}
+          (doall
+           (render-contours
+            {:contour next-contour
+             :color-scale #(.interpolateTurbo d3-scale-chromatic %)}))]]
+        (inc year)])]))
 
 (defn app []
   (when-not @surface-temperature-data
